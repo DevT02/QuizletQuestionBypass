a27ffa6cfdec20fc23c11c56b640c21559986788
DevT02 <40608267+DevT02@users.noreply.github.com>
2022-10-03 21:22:15 -0400

    initial commit

---
+++ quizletquestionbypass.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Quizlet Question Bypass
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Gives you the images of the solutions on the bottom. Works best with auto-login quizlet.
+// @author       You
+// @match        http*://www.quizlet.com/explanations/questions/*
+// @match        http*://quizlet.com/explanations/questions/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener('load', function() {
+  'use strict';
+  var paywall = document.getElementById('__NEXT_DATA__'); // get paywall
+  var data = paywall.outerHTML; // outer json html
+
+  // couldve used regex, found it annoying
+  data = data.split("https://").map(val => { return "https://"+val }).slice(1) // split urls into an array named "data"
+  for(let i = 0; i < data.length; i++){
+    data[i] = data[i].substring(0, data[i].indexOf("\""))
+  }
+
+  console.log(data)
+
+  // remove popup
+  var paywallremover = document.getElementsByClassName("hideBelow--s")
+  paywallremover[0].remove()
+
+
+  // add new container
+  var button = document.createElement("div");
+  button.setAttribute('id', 'imageContainer')
+  button.style.backgroundColor = "white";
+
+  document.body.appendChild(button);
+
+  var container = document.getElementById('imageContainer');
+  var docFrag = document.createDocumentFragment();
+
+  data.forEach(function(url, index, originalArray) {
+    var img = document.createElement('img');
+    img.src = url;
+    docFrag.appendChild(img);
+  });
+
+
+  container.appendChild(docFrag);
+
+
+
+
+}, false);
+
+
+
