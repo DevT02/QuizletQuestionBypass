f1a8e5c505c8b01e0eb4401b7c97b3bfc94ddf7e
DevT02 <40608267+DevT02@users.noreply.github.com>
2022-11-03 01:03:32 -0400

    keep the next question visible, cleaner code
    will identify bottom

--- quizletquestionbypass.js
+++ quizletquestionbypass.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Quizlet Question Bypass
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.21
 // @description  Gives you the images of the solutions on the bottom. Works best with auto-login quizlet.
 // @author       DevT02
 // @match        http*://www.quizlet.com/explanations/questions/*
@@ -26,14 +26,17 @@ window.addEventListener('load', function() {
   function dataIncludes(a1, i){
     return data[i].includes(a1)
   }
+  var selectEachElement = $("div[class^='ExplanationsSolutionCard'],div[class*=' ExplanationsSolutionCard']")
 
-  //window.alert($("div[class^='ExplanationsSolutionCard'],div[class*=' ExplanationsSolutionCard']").eq(1).children().eq(1).children().first().children().first().prop("innerHTML")) //currently removing other elements, the document appears to not be in order
-  $("div[class^='ExplanationsSolutionCard'],div[class*=' ExplanationsSolutionCard']").each(function (index){
-       $("div[class^='ExplanationsSolutionCard'],div[class*=' ExplanationsSolutionCard']").eq(index).children().eq(1).children().first().children().first().removeAttr('style').css("filter","blur(0)")
+  // Actually unblur KATEX
+  selectEachElement.each(function (index, element){
+       var isLastElement = index == selectEachElement.length - 1;
+       if (!isLastElement){
+           $(selectEachElement).eq(index).children().eq(1).children().first().children().first().removeAttr('style').css("filter","blur(0)")
+       }
   });
-
- // WORKING  $("div[class^='ExplanationsSolutionCard'],div[class*=' ExplanationsSolutionCard']").eq(1).children().eq(1).children().first().children().first().removeAttr('style').css("filter","blur(0)"); <-- (bruh the amount of debugging)
-
+  
+  // remove URLs that we found earlier but dont need.
   for(let i = 0; i < data.length; i++){
     data[i] = data[i].substring(0, data[i].indexOf("\""))
     if (dataIncludes("lateximg.png", i) || dataIncludes("quizlet.com/explanations", i) || dataIncludes("assets", i) || dataIncludes("textbook_covers", i) || dataIncludes("cache", i)){
@@ -53,6 +56,7 @@ window.addEventListener('load', function() {
        console.log(err)
    }
   // add new container
+
   var imgCont = document.createElement("div");
   imgCont.setAttribute('id', 'imageContainer')
   imgCont.style.backgroundColor = "white";
